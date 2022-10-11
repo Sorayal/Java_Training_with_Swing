6519b6ff94332eae9b59a537ae844d2fedcb4bde
Sascha Sörries <sascha.soerries@cstx-es.de>
2022-10-11 12:39:30 +0200

    added example, tried to modify converter

--- Java_Swing_Basics/SwingTest1-CelciusConverter/src/converter/MainFrame.java
+++ Java_Swing_Basics/SwingTest1-CelciusConverter/src/converter/MainFrame.java
@@ -5,29 +5,50 @@ import javax.swing.*;
 public class MainFrame extends JFrame {
 
     private JTextField textfield;
+    private JPanel panel;
+    private JLabel celsius;
+    private JLabel fahrenheit;
+    private JButton convert;
+
 
     public MainFrame() {
         super("Celsius Converter");
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         initComponents();
-
+        add(textfield);
+        add(panel);
+        add(celsius);
+        add(fahrenheit);
+        add(convert);
+        pack();
     }
 
     // https://docs.oracle.com/javase/tutorial/uiswing/learn/adjustinggui.html
 
     // TODO: Finish Converter and build logic
     private void initComponents() {
-        JPanel panel = new JPanel();
-        JTextField textfield = new JTextField();
-        JLabel celsius = new JLabel("Celsius");
-        JLabel fahrenheit = new JLabel("Fahrenheit");
-        JButton convert = new JButton("Convert");
+        this.panel = new JPanel();
+        this.textfield = new JTextField();
+        this.celsius = new JLabel("Celsius");
+        this.fahrenheit = new JLabel("Fahrenheit");
+        this.convert = new JButton("Convert");
         GroupLayout layout = new GroupLayout(panel);
-        panel.setLayout(layout);
+        this.panel.setLayout(layout);
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
 
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                //.addComponent(textfield)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
+        );
+
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
+        );
+        /*
         layout.setHorizontalGroup(
                 layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
@@ -38,5 +59,7 @@ public class MainFrame extends JFrame {
                                 .addComponent(fahrenheit)
                         )
         );
+
+         */
     }
 }
